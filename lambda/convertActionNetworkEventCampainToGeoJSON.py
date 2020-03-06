c5638cd193f9ad1582c036cda484283c032bd6f4
Mikel Maron <mikel_maron@yahoo.com>
2020-03-05 21:41:18 -0500

    updated lambda

--- lambda/convertActionNetworkEventCampainToGeoJSON.py
+++ lambda/convertActionNetworkEventCampainToGeoJSON.py
@@ -2,7 +2,8 @@ import logging
 import os
 from typing import Dict
 
-from botocore.vendored import requests
+import urllib3
+http = urllib3.PoolManager()
 
 from dateutil import parser
 from datetime import datetime, timedelta
@@ -35,15 +36,24 @@ def safestrip(string: str) -> str:
       return string.strip()
     except:
       return ""
-      
+
+def is_us(event: Dict) -> bool:
+    location = (event.get('location', {}) or {})
+    country = location.get('country')
+    return country == "US"
+
 def make_location(event: Dict) -> str:
     location = (event.get('location', {}) or {})
     # need to update to handle null values
     full_location = '{venue}, {address}, {locality}, {region} {postal_code}'.format(
-        venue = safestrip(location.get('venue')), address = safestrip(location.get('address_lines')[0]), locality = safestrip(location.get('locality')), 
+        venue = safestrip(location.get('venue')), address = safestrip(location.get('address_lines')[0]), locality = safestrip(location.get('locality')),
         region = safestrip(location.get('region')), postal_code = safestrip(location.get('postal_code'))
-        ) 
+        )
     return full_location
+
+def make_state(event: Dict) -> str:
+    location = (event.get('location', {}) or {})
+    return safestrip(location.get('region'))
 
 def make_coord(event: Dict) -> list:
     location = (event.get('location', {}) or {})
@@ -51,14 +61,23 @@ def make_coord(event: Dict) -> list:
     lat = coords.get('latitude')
     lng = coords.get('longitude')
     return [lng, lat]
-    
-event_id = 0    
+
+def make_host(event: Dict) -> str:
+    description = event.get('description')
+    if "**FAITH EVENT**" in description:
+        return "faith"
+    elif "**LABOR EVENT**" in description:
+        return "labor"
+    else:
+        return ""
+
+event_id = 0
 def make_id(event: Dict) -> str:
     #return (event.get('identifiers', [""]))[0]
     global event_id
     event_id = event_id + 1
     return event_id
-    
+
 def convert_event(event: Dict) -> Dict:
     coord = make_coord(event)
     return {
@@ -69,8 +88,10 @@ def convert_event(event: Dict) -> Dict:
         'eventDate': parser.parse(event.get('start_date', '4/22/2020')).strftime('%-m/%-d/%Y'),
         'timestamp': event.get('start_date', '2020-04-22T00:00:00Z"'),
         'eventLink': event.get('browser_url', ''),
-        'location': make_location(event)
-      },  
+        'location': make_location(event),
+        'state': make_state(event),
+        'host_type': make_host(event)
+      },
       "geometry": {
         "type": "Point",
         "coordinates": [
@@ -88,15 +109,12 @@ def get_events_from_events_campaign(return_events=None,
     log.info('\nget_events_from events_campaign (action network) -- page %d',
              page)
 
-    response = requests.get(
-        'https://actionnetwork.org/api/v2/event_campaigns/{event_campaign_id}/events?page={page}'.
-        format(
-            event_campaign_id=os.environ['ACTION_NETWORK_EVENTS_CAMPAIGN_ID'],
-            page=page),
-        headers={
-            'OSDI-API-Token': os.environ['ACTION_NETWORK_API_KEY']
-        })
-    if response.status_code != 200:
+    response = http.request('GET',
+                        'https://actionnetwork.org/api/v2/event_campaigns/' + os.environ['ACTION_NETWORK_EVENTS_CAMPAIGN_ID'] + '/events?page=' + str(page),
+                        headers={ 'OSDI-API-Token': os.environ['ACTION_NETWORK_API_KEY']},
+                        retries = False)
+
+    if response.status != 200:
         log.error(
             'ERROR\tResponse code %d received from https://actionnetwork.org/api/v2/event_campaigns',
             response.status_code)
@@ -106,11 +124,12 @@ def get_events_from_events_campaign(return_events=None,
         '\nsuccess! get_events_from events_campaign (action network) -- page %d',
         page)
 
-    response_json = response.json()
+    response_json = json.loads(response.data.decode('utf-8'))
 
     events = response_json.get('_embedded', {}).get('osdi:events', {})
     for event in events:
-        return_events.append(convert_event(event))
+        if (is_us(event)):
+            return_events.append(convert_event(event))
 
     if response_json.get('total_pages', page) > page:
         get_events_from_events_campaign(return_events, page + 1)
